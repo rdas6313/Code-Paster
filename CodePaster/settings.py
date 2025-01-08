58c1e7ac9f6b6d9651f83e98c9deb06cc5d17381
Raja <rdas6313@gmail.com>
2025-01-08 18:16:24 +0530

    fix(settings): resolve migration issue related to installed app and authentication model

--- CodePaster/settings.py
+++ CodePaster/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "account",
+    "pasteapp",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# custom authentication model
+AUTH_USER_MODEL = "account.User"
